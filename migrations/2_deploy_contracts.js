21fd5b928980a8c5e938b2fc0f8ed4c61f2d28b8
Antoine Rondelet <rondelet.antoine@gmail.com>
2018-04-25 23:04:23 +0100

    Added TransactionRelay to migration file

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,7 @@
 const KeyManager = artifacts.require("./KeyManager.sol");
+const TransactionRelay = artifacts.require("./TransactionRelay.sol");
 
 module.exports = (deployer) => {
   deployer.deploy(KeyManager);
+  deployer.deploy(TransactionRelay);
 };
