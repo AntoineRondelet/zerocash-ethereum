3285579edde8df8f6df7ce2eb0739d178bc73004
Antoine Rondelet <rondelet.antoine@gmail.com>
2018-04-25 22:42:08 +0100

    Added development network to truffle config file

--- truffle.js
+++ truffle.js
@@ -1,4 +1,15 @@
 module.exports = {
-  // See <http://truffleframework.com/docs/advanced/configuration>
-  // to customize your Truffle configuration!
+  networks: {
+    development: {
+      host: "localhost",
+      port: 8545,
+      network_id: "*"
+    },
+  },
+  solc: {
+    optimizer: {
+      enabled: true,
+        runs: 200
+    }
+  }
 };
