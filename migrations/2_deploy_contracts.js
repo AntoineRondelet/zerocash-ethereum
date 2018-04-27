e2f4e6ae14724fb8a06e07df0f7a4f52e8377a87
Antoine Rondelet <rondelet.antoine@gmail.com>
2018-04-27 19:42:49 +0100

    Added coinProvider to deploy contract migration

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,7 +1,9 @@
 const KeyManager = artifacts.require("./KeyManager.sol");
 const TransactionRelay = artifacts.require("./TransactionRelay.sol");
+const CoinProvider = artifacts.require("./CoinProvider.sol");
 
 module.exports = (deployer) => {
   deployer.deploy(KeyManager);
   deployer.deploy(TransactionRelay);
+  deployer.deploy(CoinProvider);
 };
