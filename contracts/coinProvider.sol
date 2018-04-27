8b4ae3e4fc29c3527dbbd26f8ba48e792691e53a
Antoine Rondelet <rondelet.antoine@gmail.com>
2018-04-27 19:42:15 +0100

    Deleted serial number from coin

--- contracts/coinProvider.sol
+++ contracts/coinProvider.sol
@@ -1,11 +1,9 @@
 pragma solidity ^0.4.19;
 
 contract CoinProvider {
-
     struct Coin {
-        bytes32 serialNumber;
         bytes32 hashOfSecret;
-        uint32 value;
+        uint256 value;
         bool isValid;
     }
     
@@ -19,22 +17,23 @@ contract CoinProvider {
         revert();
     }
 
-    function createCoin(uint32 denomination, bytes32 hashOfSecret) public payable returns (bytes32) {
-        bytes32 serialNumber = keccak256(msg.sender, now);
-        commitList[serialNumber] = Coin({
-            serialNumber: serialNumber, 
+    function createCoin(uint256 denomination, bytes32 hashOfSecret) public payable returns (bool) {
+        require(msg.value == denomination);
+        commitList[hashOfSecret] = Coin({
             hashOfSecret: hashOfSecret,
             value: denomination,
             isValid: true
         });
 
-        return serialNumber;
+        return true;
     }
 
-    function claimFunds(bytes32 serialNumber, uint256 secretPreImage) returns (bool) {
-        require(keccak256(secretPreImage) == commitList[serialNumber].hashOfSecret);
-        msg.sender.transfer(commitList[serialNumber].value);
-        commitList[serialNumber].isValid = false; // prevent to redeem the same coin multiple times
+    function claimFunds(uint256 secretPreImage) public constant returns (bool) {
+        bytes32 computedHash = keccak256(secretPreImage);
+        require(computedHash == commitList[computedHash].hashOfSecret);
+        msg.sender.transfer(commitList[computedHash].value);
+        commitList[computedHash].isValid = false; // prevent to redeem the same coin multiple times
+
         return true;
     }
 
