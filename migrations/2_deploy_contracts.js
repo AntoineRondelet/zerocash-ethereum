ea15d94d61d46fabee7e7b58cc72059f2a04cc3f
Antoine Rondelet <rondelet.antoine@gmail.com>
2018-05-07 19:29:36 +0100

    Migration for the verifier and factorize contracts

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,15 +1,13 @@
 //const KeyManager = artifacts.require("./KeyManager.sol");
 //const TransactionRelay = artifacts.require("./TransactionRelay.sol");
 //const CoinProvider = artifacts.require("./CoinProvider.sol");
-const SumsToFifteenDebug = artifacts.require("./SumsToFifteenDebug.sol");
-//const Verifier = artifacts.require("./Verifier.sol");
-//const Factorize = artifacts.require("./factorize.sol");
+const Verifier = artifacts.require("./Verifier.sol");
+const Factorize = artifacts.require("./factorize.sol");
 
 module.exports = (deployer) => {
   //deployer.deploy(KeyManager);
   //deployer.deploy(TransactionRelay);
   //deployer.deploy(CoinProvider);
-  //deployer.deploy(Verifier);
-  //deployer.deploy(Factorize);
-  deployer.deploy(SumsToFifteenDebug);
+  deployer.deploy(Verifier);
+  deployer.deploy(Factorize);
 };
