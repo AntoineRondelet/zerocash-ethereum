515ccaf2657fd6543d51a546d07ec4a1bf2bf778
Antoine Rondelet <rondelet.antoine@gmail.com>
2018-05-07 15:35:50 +0100

    Added debug contract and debug migration

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,13 +1,15 @@
-const KeyManager = artifacts.require("./KeyManager.sol");
-const TransactionRelay = artifacts.require("./TransactionRelay.sol");
-const CoinProvider = artifacts.require("./CoinProvider.sol");
-const Verifier = artifacts.require("./verifier.sol");
-const Factorize = artifacts.require("./factorize.sol");
+//const KeyManager = artifacts.require("./KeyManager.sol");
+//const TransactionRelay = artifacts.require("./TransactionRelay.sol");
+//const CoinProvider = artifacts.require("./CoinProvider.sol");
+const SumsToFifteenDebug = artifacts.require("./SumsToFifteenDebug.sol");
+//const Verifier = artifacts.require("./Verifier.sol");
+//const Factorize = artifacts.require("./factorize.sol");
 
 module.exports = (deployer) => {
-  deployer.deploy(KeyManager);
-  deployer.deploy(TransactionRelay);
-  deployer.deploy(CoinProvider);
-  deployer.deploy(Verifier);
-  deployer.deploy(Factorize);
+  //deployer.deploy(KeyManager);
+  //deployer.deploy(TransactionRelay);
+  //deployer.deploy(CoinProvider);
+  //deployer.deploy(Verifier);
+  //deployer.deploy(Factorize);
+  deployer.deploy(SumsToFifteenDebug);
 };
